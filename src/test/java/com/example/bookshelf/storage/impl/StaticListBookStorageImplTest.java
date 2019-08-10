69b308826b77f1aa8e6f70fc3f8db13a74753e32
PatrykIgras <Patrys90>
2019-08-11 00:15:37 +0200

    Repair #2

--- src/test/java/com/example/bookshelf/storage/impl/StaticListBookStorageImplTest.java
+++ src/test/java/com/example/bookshelf/storage/impl/StaticListBookStorageImplTest.java
@@ -1,7 +1,6 @@
 package com.example.bookshelf.storage.impl;
 
 import com.example.bookshelf.type.Book;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
